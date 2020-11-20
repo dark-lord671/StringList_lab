fc608c5ef28161c199e13eb3db059f3b6152e5d9
Михаил Оридорога <mihacraft314@gmail.com>
2020-11-20 10:42:09 +0200

    + class String + struct ListNode + class StringList +  StringList constructor +  StringList destructor +  RemoveHead + AddHead

---
+++ Lab_3VS/StringList.cpp
@@ -0,0 +1,71 @@
+#include "StringList.h"
+using namespace std;
+
+// Construct an empry list for ListNode objs.
+StringList::StringList(void) {
+    size = 0;
+    head = nullptr;
+}
+
+// Delete a list
+StringList::~StringList() {
+    while (size)
+    {
+        this->RemoveHead();
+    }
+}
+
+//Removes the element from the head of the list.
+void StringList::RemoveHead(void)
+{
+    if (head)
+    {
+        ListNode* tmp = head->next;
+        delete head;
+        head = tmp;
+        size--;
+    }
+}
+
+//Adds an element to the head of the list (makes a new head).
+void StringList::AddHead(const char* str)
+{
+    if (!head) {
+        head = (ListNode*) malloc(sizeof(ListNode));
+        if (!head) {
+            throw runtime_error("Error : Allocanion error AddHead");
+        }
+        size++;
+        head->data = str;
+    }
+    else {
+        ListNode* tmp = (ListNode*)malloc(sizeof(ListNode));
+        if (!head) {
+            throw runtime_error("Error : Allocanion error AddHead");
+        }
+        tmp->next = head;
+        tmp->next->prev = tmp;
+        tmp->data = str;
+        head = tmp;
+    }
+}
+
+
+
+
+
+
+
+int main() {
+    try
+    {
+        StringList lst;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+
+    return 0;
+}
---
+++ Lab_3VS/StringList.h
@@ -0,0 +1,146 @@
+/*
+ * file: StringList.h
+ * StringList class declaration
+ * created: 19/11/2020
+ * last changes: xx/xx/xxxx
+ * Copyright (c) by Orydoroha Mykhailo
+ */
+
+#ifndef __STRINGLIST_H__
+#define __STRINGLIST_H__
+
+#include <iostream>
+#include <string.h>
+#include <malloc.h>
+
+
+class String {
+public:
+    String(void)
+        : size(0) {
+        data_ = nullptr;
+    }
+
+    String(const char* str)
+        : size(strlen(str))
+    {
+        data_ = (char*)malloc(size + 1);
+        if (!data_) {
+            throw std::runtime_error("Error : String memory allocation error \n");
+        }
+    }
+
+    String& operator=(const char* str)
+    {
+        if(!data_)
+			delete data_;
+        size = strlen(str);
+        data_ = (char*)malloc(size + 1);
+        return *this;
+    }
+
+    ~String() {
+        std::cout << "String Destructon \n";
+        free(data_);
+    }
+
+    sizet size(void) const {
+        return this->size;
+    }
+private:
+    char* data_;
+    sizet size;
+};
+
+struct ListNode {
+    String data = "";
+    ListNode* next = nullptr;
+    ListNode* prev = nullptr;
+};
+
+class StringList {
+    // typedef const ListNode* POSITION;
+    using POSITION = ListNode*;
+private:
+    //put your own data members here
+    sizet size;
+    ListNode* head;
+public:
+    // Construct an empry list for ListNode objs.
+    StringList(void);
+    ~StringList();
+    //Head/Tail Access
+    const ListNode* GetHead();//Returns the head element of the list
+    const ListNode* GetTail();//Returns the tail element of the list
+
+    //Operations
+    //Adds an element to the head of the list (makes a new head).
+    void AddHead(const char*);
+
+    //Adds all the elements in another list to the head of the list (makes a new head).
+    void AddHead(const StringList*);
+
+    //Adds an element to the tail of the list (makes a new tail).
+    void AddTail(const char*);
+
+    //Adds all the elements in another list to the tail of the list (makes a new tail).
+    void AddTail(const StringList*);
+
+    //Removes all the elements from this list.
+    void RemoveAll();
+
+    //Removes the element from the head of the list.
+    void RemoveHead();
+
+    //Removes the element from the tail of the list.
+    void RemoveTail();
+    void AppendExclusively(const StringList*);
+    void Splice(POSITION where, StringList* sl, POSITION first, POSITION last);
+
+    //removes all duplicate elements
+    void Unique();
+
+    //Iteration
+    //Gets the next element for iterating.
+    POSITION GetNext();
+
+    //Gets the previous element for iterating.
+    POSITION GetPrev();
+
+    //Retrieval/Modification
+    POSITION GetHeadPosition();
+
+    //Gets the element at a given position.
+    const char* GetAt(int)const;
+
+    //Removes an element from this list as specified by position.
+    void RemoveAt(int);
+
+    //Sets the element at a given position.
+    void SetAt(char*, int);
+
+    //Insertion
+    //Inserts a new element after a given position.
+    void InsertAfter(char*, int);
+
+    //Inserts a new element before a given position.
+    void InsertBefore(char*, int);
+
+    //Searching
+    //Gets the position of an element specified by string value.
+    const ListNode* Find(char*);
+
+    //Gets the position of an element specified by a zero-based index.
+    int FindIndex(char*)const;
+
+    //Status
+    //Returns the number of elements in this list.
+    int Getsize()const;
+
+    //Tests for the empty list condition (no elements).
+    bool IsEmpty()const;
+    void Printnode(const ListNode* p);
+
+};
+
+#endif //!__STRINGLIST_H__
